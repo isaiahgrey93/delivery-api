31808b25e453ff02d54e20c35d2eeaae40ba9402
isaiahgrey93 <isaiahgrey@gmail.com>
2016-07-05 12:45:27 -0700

    basic auth scheme setup with hapi-cookie-auth, api plugin setup

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 The server is configured via the following environment variables, which should be placed in a .env file in the project root.
 
+- DB_CONNECTION_STRING - {Database} connection string
+
 ## NPM Scripts
 
 - `start` - start the server
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const Hapi = require('hapi');
+const HapiAuthCookie = require('hapi-auth-cookie');
 const Dotenv = require('dotenv');
 const Path = require('path');
+const Api = require('./lib');
 const server = new Hapi.Server();
 
 Dotenv.config({ path: Path.resolve(__dirname, '.env') });
@@ -11,6 +13,15 @@ server.connection({ port: 3000 });
 
 server.register([
   // plugins
+  HapiAuthCookie,
+  {
+    register: Api,
+    options: {
+      db: {
+        url: process.env.DB_CONNECTION_STRING
+      }
+    }
+  }
 ], (err) => {
   if (err) {
     throw err;
