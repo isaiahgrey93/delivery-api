1612346fe657cf49523639060830509e24d80e59
isaiahgrey93 <isaiahgrey@gmail.com>
2017-07-12 13:06:14 -0700

    clean up utils

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,4 @@
-"use strict";
-
 module.exports = {
-    model: require("./model"),
     user: require("./user"),
-    stripe: require("./stripe"),
-    drive: require("./drive")
+    stripe: require("./stripe")
 };
